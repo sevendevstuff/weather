17f4da017cd5e4c55d2afcc7c4b62b677992b536
Seven <albustefan2008@gmail.com>
2024-09-26 21:19:52 +0300

    init test

---
+++ src/main/java/com/stefan/weather/db/City.java
@@ -0,0 +1,17 @@
+package com.stefan.weather.db;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    public Double lat;
+    public Double lng;
+
+    @Column(unique = true, nullable = false)
+    public String name;
+}
